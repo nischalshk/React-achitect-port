b9ac76fed29b69fc261d0c008fbd7c637ac4c0b2
nischalshk <nischal.shk@gmail.com>
2021-01-19 20:59:17 +0545

    Nav&Slider done

---
+++ ShoppingList/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {View, Text, Stylesheet} from 'react-native';
+
+function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello World</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    color: 'darkslateblue',
+    fontSize: 30,
+  },
+});
+
+export default App;
---
+++ react-architect-port-v1/src/data/SliderData.js
@@ -0,0 +1,70 @@
+import ImageOne from '../images/home1.jpg';
+import ImageTwo from '../images/home2.jpg';
+import ImageThree from '../images/home3.jpg';
+import ImageFour from '../images/home4.jpg';
+
+export const SliderData = [
+  {
+    title: 'Luxury Bungalow Design',
+    description: 'asdf',
+    location: '',
+    image: ImageOne,
+    path: '/designs',
+    alt: 'Designs',
+    label: 'View Designs',
+  },
+  {
+    title: 'Minimal Family Home',
+    description: 'asdf',
+    location: '',
+    image: ImageTwo,
+    path: '/designs',
+    alt: 'Designs',
+    label: 'View Designs',
+  },
+  {
+    title: 'Luxury Bungalow Design',
+    description: 'asdf',
+    location: '',
+    image: ImageThree,
+    path: '/designs',
+    alt: 'Designs',
+    label: 'View Designs',
+  },
+  {
+    title: 'Blueprint Design',
+    description: 'asdf',
+    location: '',
+    image: ImageFour,
+    path: '/designs',
+    alt: 'Designs',
+    label: 'View Designs',
+  },
+];
+
+// export const SliderDataTwo = [
+//   {
+//     title: 'Luxury Bungalow Design',
+//     description: '',
+//     location: '',
+//     image: '',
+//     path: '/designs',
+//     alt: 'Designs',
+//   },
+//   {
+//     title: 'Minimal Family Home',
+//     description: '',
+//     location: '',
+//     image: '',
+//     path: '/designs',
+//     alt: 'Designs',
+//   },
+//   {
+//     title: 'Luxury Bungalow Design',
+//     description: '',
+//     location: '',
+//     image: '',
+//     path: '/designs',
+//     alt: 'Designs',
+//   },
+// ];
